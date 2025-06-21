08abfe3555438adb3e5955e6ae22bf2f115fcd6d
Chad Collins <chad_a_collins@phpthis.com>
2025-06-21 10:25:28 -0500

    Removed firtstname andlastname from createCustomerDto

--- Models/Dtos/CreateCustomerDto.cs
+++ Models/Dtos/CreateCustomerDto.cs
@@ -3,8 +3,6 @@
     public class CreateCustomerDto
     {
         public int Id { get; set; } 
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
         public string Email { get; set; } = null!;
 
     }
