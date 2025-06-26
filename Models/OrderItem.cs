4e3de03dac9a6ce3a36546865624196f42dbf9b6
Chad Collins <chad_a_collins@phpthis.com>
2025-06-26 12:16:59 -0500

    Adding the OrderItems.Orders collection property back but with a JsonIgnore decorator. Going to test to make sure it doesn't cause a circular response issue.

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,11 +1,14 @@
-﻿namespace HighThroughputApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace HighThroughputApi.Models
 {
     public class OrderItem
     {
         public int Id { get; set; } 
 
-        public int OrderId { get; set; }  
-        //public Order Order { get; set; } = null!;
+        public int OrderId { get; set; }
+        [JsonIgnore]
+        public Order Order { get; set; } = null!;
 
         public int ItemId { get; set; }  
         public Item Item { get; set; } = null!;
