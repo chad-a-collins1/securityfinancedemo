e907bb92d05bbcb656d55dc7fc0572ae3b644de2
Chad Collins <chad_a_collins@phpthis.com>
2025-06-26 12:10:07 -0500

    Removing the Orders collection property from OrderItem to prevent a circular reference.

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; } 
 
         public int OrderId { get; set; }  
-        public Order Order { get; set; } = null!;
+        //public Order Order { get; set; } = null!;
 
         public int ItemId { get; set; }  
         public Item Item { get; set; } = null!;
